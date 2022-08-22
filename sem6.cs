38a4a51677032e5b840bd04159b4fb1b7b09b040
iGolubS <109303592+iGolubS@users.noreply.github.com>
2022-08-23 00:03:06 +0300

    Update sem6.cs

--- sem6.cs
+++ sem6.cs
@@ -1,100 +1,70 @@
-//Напишите программу, которая перевернёт одномерный массив 
-//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
-Console.Clear();
+// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
 
-int[] array = GetArray(10, 0, 10);
-Console.WriteLine(String.Join(" ", array));
+// Console.Clear();
 
-int[] reversArray=ReversArray2(array);
-Console.WriteLine(String.Join(" ", reversArray));
+// int[] array = GetArray(10, 0, 10);
+// Console.WriteLine(String.Join(" ", array));
 
-ReversArray1(array);
-Console.WriteLine(String.Join(" ", array));
+// int[] reversArray=ReversArray2(array);
+// Console.WriteLine(String.Join(" ", reversArray));
 
-int[] GetArray(int size, int minValue, int maxValue)
-{
-    int[] res = new int[size];
+// ReversArray1(array);
+// Console.WriteLine(String.Join(" ", array));
 
-    for (int i = 0; i < size; i++)
-    {
-        res[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return res;
-}
+// int[] GetArray(int size, int minValue, int maxValue)
+// {
+//     int[] res = new int[size];
 
-void ReversArray1(int[] inArray)
-{
-    for (int i = 0; i < inArray.Length / 2; i++)
-    {
-        int k = inArray[i];
-        inArray[i] = inArray[inArray.Length - i - 1];
-        inArray[inArray.Length - i - 1] = k;
-    }
-}
+//     for (int i = 0; i < size; i++)
+//     {
+//         res[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return res;
+// }
 
-int[] ReversArray2(int[] inArray)
-{
-    int[] result = new int[inArray.Length];
-    for (int i = 0; i < inArray.Length; i++)
-    {
-        result[i] = inArray[inArray.Length - 1 - i];
+// void ReversArray1(int[] inArray)
+// {
+//     for (int i = 0; i < inArray.Length / 2; i++)
+//     {
+//         int k = inArray[i];
+//         inArray[i] = inArray[inArray.Length - i - 1];
+//         inArray[inArray.Length - i - 1] = k;
+//     }
+// }
 
-    }
-    return result;
-}
+// int[] ReversArray2(int[] inArray)
+// {
+//     int[] result = new int[inArray.Length];
+//     for (int i = 0; i < inArray.Length; i++)
+//     {
+//         result[i] = inArray[inArray.Length - 1 - i];
 
-
-
+//     }
+//     return result;
+// }
 
 
 
 
 
+// Может ли получится треугольник из саданных 3х точек?
 
-
-
-
-
-
-Console.Clear();
-Console.WriteLine("Введите число А");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число B");
-int B = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число C");
-int C = Convert.ToInt32(Console.ReadLine());
-
-if (A < B + C && B < A + C && C < A + B)
-{
-    Console.WriteLine("треугольник может существовать");
-}
-else
-{
-    Console.WriteLine("треугольника нет");
-}
-
-
-
-
-
-
-
-
-
-// Задача 44 Не используйте рекурсию , введите .
-
-
-int Nextfib(int praded, int ded)
-{
-    return praded + ded;
-}
-Console.Write("Введите первое число : ");
-int num = int.Parse(Console.ReadLine());
-int[] array = new int[num];
- array[0]= 0;
- array[1]= 1;
-
-
+// Console.Clear();
+// Console.WriteLine("Введите число А");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число B");
+// int B = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число C");
+// int C = Convert.ToInt32(Console.ReadLine());
+// if (A < B + C && B < A + C && C < A + B)
+// {
+//     Console.WriteLine("треугольник может существовать");
+// }
+// else
+// {
+//     Console.WriteLine("треугольника нет");
+// }
 
 
 
@@ -103,75 +73,21 @@ int[] array = new int[num];
 //Напишите программу, которая принимает на вход три числа и проверяет, 
 //может ли существовать треугольник с сторонами такой длины.
 
-Console.Clear();
-Console.Write("Введите стороны треугольника через пробел: ");
+// Console.Clear();
+// Console.Write("Введите стороны треугольника через пробел: ");
 
-string[] st = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+// string[] st = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
 
-if(IsTriangle(int.Parse(st[0]),int.Parse(st[1]),int.Parse(st[2])))
-{
-    Console.WriteLine("Да");
-}
-else
-{
-    Console.WriteLine("Нет");
-}
-
-
-
-bool IsTriangle(int a,int b,int c)
-{
-    return (((a+b)>c)&&((a+c)>b)&&((b+c)>a));
-}
-
-
-
-
-
-
-// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-// 45 -> 101101
-// 3  -> 11
-// 2  -> 10
-
-// Console.WriteLine("Введите число N");
-// int N = Convert.ToInt32(Console.ReadLine());
-// int x = 1;
-// while (N > 0)
+// if(IsTriangle(int.Parse(st[0]),int.Parse(st[1]),int.Parse(st[2])))
 // {
-//     if (N % 2 == 0)
-//     {
-//         x++;
-//         N = N / 2;
-//     }
-//     else
-//     {
-//         x++;
-//         N = N / 2;
+//     Console.WriteLine("Да");
 // }
-// }
-
-// int [] array = new int[x];
-// int I = 0;
-// while (N > 0)
+// else
 // {
-//     if (N % 2 == 0)
-//     {
-//         array[I] = 0;
-//         I++;
-//         N = N / 2;
-//     }
-//     else
-//     {
-//         array[I] = 1;
-//         I++;
-//         N = N / 2;156
-// }
+//     Console.WriteLine("Нет");
 // }
 
-// Console.Write(array);
-
-
-
-
-
+// bool IsTriangle(int a,int b,int c)
+// {
+//     return (((a+b)>c)&&((a+c)>b)&&((b+c)>a));
+// }
